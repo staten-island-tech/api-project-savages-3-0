0a8c1dfa30e8d6fdd0791b01e9fc0ffe1bb8218f
vincent1598 <vincent.he0403@gmail.com>
2021-05-17 02:54:18 -0400

    api

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
-import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
-
-const key = "YOURKEYHERE";
+const query = async function () {
+  try {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/filter.php?c=vegan"
+    );
+    const data = await response.json();
+    console.log(data);
+    const apiTest = data;
+    document.getElementById("test").innerText = apiTest;
+  } catch (error) {
+    console.log(error);
+    alert("Hey something went wrong");
+  }
+};
+query();
