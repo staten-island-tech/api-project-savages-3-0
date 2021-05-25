1fd69eb54b4437535dc2807eb90433a43515c1e3
rachhely <71524300+rachhely@users.noreply.github.com>
2021-05-25 01:01:13 -0400

    yes

---
+++ src/js/DOM.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+  grid: document.querySelector(".recipe-grid"),
+  searchForm: document.getElementById("search-from"),
+  searchArea: document.getElementById("search-area"),
+};
+
+export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,37 @@
+import { DOMSelectors } from "./DOM";
+
 const query = async function () {
   try {
     const response = await fetch(
       "https://www.themealdb.com/api/json/v1/1/filter.php?c=vegan"
     );
     const data = await response.json();
-    console.log(data);
-    const apiTest = data.meals;
-    document.getElementById("test").innerText = apiTest;
+    data.meals.forEach((recipe) => {
+      console.log(recipe.meals);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="recipe-card">
+      <div class="recipe-card-front">
+        <img
+          src="https://image.tmdb.org/t/p/w300/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg"
+          alt=""
+          class="thumbnail"
+        />
+      </div>
+      <div class="recipe-card-back">
+        <h3 class="recipe-card-header">${recipe.strMeal}</h3>
+        <div class="recipe-types">
+          <li class="recipe-type">Vegan</li>
+          <li class="recipe-type">Fantasy</li>
+          <li class="recipe-type">Horror</li>
+        </div>
+      </div>
+    </div>`
+      );
+    });
   } catch (error) {
     console.log(error);
     alert("Hey something went wrong");
   }
 };
-query();
+//query();
