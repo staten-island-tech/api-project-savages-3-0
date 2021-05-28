9080d712fef890660f1295d64bf204b6bfed09e6
vincent1598 <vincent.he0403@gmail.com>
2021-05-28 04:43:36 -0400

    ?

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,8 @@
 const DOMSelectors = {
   grid: document.querySelector(".recipe-grid"),
+  searchForm: document.getElementById("search-form"),
+  searchArea: document.getElementById("search-area"),
+  ingredientList: document.getElementsByClassName(".recipe-ingredients"),
 };
 
 export { DOMSelectors };
